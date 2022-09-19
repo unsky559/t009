59ea8d49ba486f4cb112d53395da34768130f754
unsky559 <6kostya@gmail.com>
2022-09-20 01:01:43 +0300

    feat: delete film dialog

--- src/componetns/film/filmCard.tsx
+++ src/componetns/film/filmCard.tsx
@@ -22,12 +22,14 @@ type propType = {
 }
 
 const FilmCard = (props: propType) => {
-  const rq = useFetch(FilmService.delete({ id: props.film.id }));
+  const [hideDeleted, setHideDeleted] = useState(false);
+  const deleterq = useFetch(FilmService.delete({ id: props.film.id }));
   const rqInfo = useFetch<{ data: IFilm }>(FilmService.show({ id: props.film.id }));
   const [dialogOpen, updateDialogOpen] = useState(false);
+  const [dialogDeleteOpen, updateDialogDeleteOpen] = useState(false);
 
   const deleteClick = () => {
-    rq.fetching();
+    updateDialogDeleteOpen(true);
   };
 
   const infoClick = () => {
@@ -45,7 +47,7 @@ const FilmCard = (props: propType) => {
     }
   }, [rqInfo.data]);
 
-  return !rq.data && (
+  return !hideDeleted && (
       <>
           <Backdrop
               sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
@@ -53,6 +55,40 @@ const FilmCard = (props: propType) => {
           >
               <CircularProgress color="inherit" />
           </Backdrop>
+          <Dialog
+              open={dialogDeleteOpen}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+          >
+              <DialogTitle id="alert-dialog-title">
+                  {deleterq.data ? 'Film was successfully deleted' : 'Are you sure you want to delete this film?'}
+              </DialogTitle>
+              <DialogContent>
+                  {deleterq.isLoading && <CircularProgress/>}
+              </DialogContent>
+              <DialogActions>
+                  {!deleterq.data
+                      && <>
+                          <Button
+                              disabled={deleterq.isLoading}
+                              onClick={() => deleterq.fetching()} autoFocus>
+                              delete
+                          </Button>
+                          <Button
+                              disabled={deleterq.isLoading}
+                              onClick={() => updateDialogDeleteOpen(false)} autoFocus>
+                              cancel
+                          </Button>
+                      </>
+                  }
+                  {deleterq.data
+                      && <Button
+                          onClick={() => setHideDeleted(true)} autoFocus>
+                          ok
+                      </Button>
+                  }
+              </DialogActions>
+          </Dialog>
           <Dialog
               open={dialogOpen}
               aria-labelledby="alert-dialog-title"
@@ -93,11 +129,11 @@ const FilmCard = (props: propType) => {
                   <CardActions>
                       <Button
                           onClick={infoClick}
-                          disabled={rq.isLoading || rqInfo.isLoading}>
+                          disabled={deleterq.isLoading || rqInfo.isLoading}>
                           About film
                       </Button>
                       <Button
-                          disabled={rq.isLoading || rqInfo.isLoading}
+                          disabled={deleterq.isLoading || rqInfo.isLoading}
                           onClick={deleteClick}>
                           Delete
                       </Button>
